40a683afdacbf56f49acd0238d1e12d6ea6e000f
asim <asim.shehzad@prowise.com>
2021-06-03 15:39:38 +0200

    test commit

--- app/src/main/java/com/amaze/filemanager/ui/activities/MainActivity.java
+++ app/src/main/java/com/amaze/filemanager/ui/activities/MainActivity.java
@@ -186,6 +186,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
+// This is Fork project
+
 public class MainActivity extends PermissionsActivity
     implements SmbConnectionListener,
         BookmarkCallback,
